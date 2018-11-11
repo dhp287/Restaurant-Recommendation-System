6a78ac8b4c1d55f6325ae44ec0647b14e1315297
Dhaval Pathak <dhp287@nyu.edu>
2018-11-11 16:55:13 -0500

    'changes'

--- Lambdas/chatbotAPICall.py
+++ Lambdas/chatbotAPICall.py
@@ -13,61 +13,61 @@ def lambda_handler(event, context):
         MessageAttributeNames=['All'],
         MaxNumberOfMessages=1
     )
+    for k in range(len(resp)):
+        msg = resp['Messages'][k]
+        receipt_handle = msg['ReceiptHandle']
 
-    msg = resp['Messages'][0]
-    receipt_handle = msg['ReceiptHandle']
+        attr = msg['MessageAttributes']
+        loc = attr['Location']
+        cuisine = attr['Cuisine']
+        contact = attr['Contact']
+        time = attr['DiningTime']
+        no = attr['NumberOfPeople']
 
-    attr = msg['MessageAttributes']
-    loc = attr['Location']
-    cuisine = attr['Cuisine']
-    contact = attr['Contact']
-    time = attr['DiningTime']
-    no = attr['NumberOfPeople']
+        ts = str(date.today().strftime("%m/%d/%Y")) + ' ' + time['StringValue'] + ':00'
+        d = datetime.strptime(ts, '%m/%d/%Y %H:%M:%S')
+        t = d.strftime("%s")
+        url = 'https://api.yelp.com/v3/businesses/search?' + 'location=' + str(loc['StringValue']) + '&term=' + str(cuisine['StringValue']) + '&open_at=' + t + '&limit=' + str(3)
+        headers = {"Authorization":"Bearer 2F6dTULd-mbARAOcjat5mbQCBpb9_dDVZ8yd_DdpgNfzfQtzzxL7IyQYQEXDfH6fqejFBM3TUgynbDvC7QT3X6zNB9Iu-rizfxjYFVlpSF14M_W3zQD_RxknX7nnW3Yx"}
+        res = requests.get(url, headers=headers)
 
-    ts = str(date.today().strftime("%m/%d/%Y")) + ' ' + time['StringValue'] + ':00'
-    d = datetime.strptime(ts, '%m/%d/%Y %H:%M:%S')
-    t = d.strftime("%s")
-    url = 'https://api.yelp.com/v3/businesses/search?' + 'location=' + str(loc['StringValue']) + '&term=' + str(cuisine['StringValue']) + '&open_at=' + t + '&limit=' + str(3)
-    headers = {"Authorization":"Bearer 2F6dTULd-mbARAOcjat5mbQCBpb9_dDVZ8yd_DdpgNfzfQtzzxL7IyQYQEXDfH6fqejFBM3TUgynbDvC7QT3X6zNB9Iu-rizfxjYFVlpSF14M_W3zQD_RxknX7nnW3Yx"}
-    res = requests.get(url, headers=headers)
+        sms = ''
+        c = 1
+        data = res.json()
+        sms += 'We found ' + str(len(data['businesses'])) + ' restaurants for you:\n\n'
+        for i in data['businesses']:
+            sms += str(c) + '. Name: ' + str(i['name']) + '.\n'
+            sms += 'Url: ' + str(i['url']) + '\n'
+            sms += 'Rating: ' + str(i['rating']) + '\n'
+            sms += 'Phone: ' + str(i['display_phone']) + '\n'
+            sms += 'Address: '
+            for j in i['location']['display_address']:
+                sms += str(j) + ' '
+            sms += '\n\n'
+            c += 1
 
-    sms = ''
-    c = 1
-    data = res.json()
-    sms += 'We found ' + str(len(data['businesses'])) + ' restaurants for you:\n\n'
-    for i in data['businesses']:
-        sms += str(c) + '. Name: ' + str(i['name']) + '.\n'
-        sms += 'Url: ' + str(i['url']) + '\n'
-        sms += 'Rating: ' + str(i['rating']) + '\n'
-        sms += 'Phone: ' + str(i['display_phone']) + '\n'
-        sms += 'Address: '
-        for j in i['location']['display_address']:
-            sms += str(j) + ' '
-        sms += '\n\n'
-        c += 1
+        dynamodb = boto3.resource('dynamodb')
 
-    dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('chatbotTable')
 
-    table = dynamodb.Table('chatbotTable')
+        r = table.scan()
+        id = len(r['Items']) + 1
 
-    r = table.scan()
-    id = len(r['Items']) + 1
+        response = table.put_item(
+        Item={
+            'id': id,
+            'contact': contact['StringValue'],
+            'sms': sms
 
-    response = table.put_item(
-    Item={
-        'id': id,
-        'contact': contact['StringValue'],
-        'sms': sms
+        }
+        )
 
-    }
-    )
+        client = boto3.client("sns", region_name="us-east-1")
+        client.publish(PhoneNumber="+1"+str(contact['StringValue']), Message=sms)
 
-    client = boto3.client("sns", region_name="us-east-1")
-    client.publish(PhoneNumber="+1"+str(contact['StringValue']), Message=sms)
+        sqs_client.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
 
-    sqs_client.delete_message(
-        QueueUrl=queue_url,
-        ReceiptHandle=receipt_handle
-    )
-
-    print('Done')
+        print('Done')
